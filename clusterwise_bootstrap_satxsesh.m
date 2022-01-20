903098e505ee8cccd8dd148e58f85842ac3bb445
ste-ioan <72309874+ste-ioan@users.noreply.github.com>
2022-01-20 13:44:14 +0100

    Add files via upload

---
+++ clusterwise_bootstrap_satxsesh.m
@@ -0,0 +1,230 @@
+%-----------------------------------------------------------------------
+% Job saved on 11-Oct-2021 15:58:41 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.tools.swe.smodel.dir = {'E:\2ndlevel\LOCA\sandwich_betas'};
+%%
+matlabbatch{1}.spm.tools.swe.smodel.scans = {
+                                             'E:\TDT_MRI\1\outputs\LOCA_contrast\baseline\beta_0001.nii,1'
+                                             'E:\TDT_MRI\2\outputs\LOCA_contrast\baseline\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\3\outputs\LOCA_contrast\baseline\beta_0001.nii,1'
+                                             'E:\TDT_MRI\4\outputs\LOCA_contrast\baseline\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\5\outputs\LOCA_contrast\baseline\beta_0001.nii,1'
+                                             'E:\TDT_MRI\6\outputs\LOCA_contrast\baseline\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\7\outputs\LOCA_contrast\baseline\beta_0001.nii,1'
+                                             'E:\TDT_MRI\8\outputs\LOCA_contrast\baseline\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\9\outputs\LOCA_contrast\baseline\beta_0001.nii,1'
+                                             'E:\TDT_MRI\10\outputs\LOCA_contrast\baseline\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\11\outputs\LOCA_contrast\baseline\beta_0001.nii,1'
+                                             'E:\TDT_MRI\12\outputs\LOCA_contrast\baseline\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\13\outputs\LOCA_contrast\baseline\beta_0001.nii,1'
+                                             'E:\TDT_MRI\14\outputs\LOCA_contrast\baseline\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\15\outputs\LOCA_contrast\baseline\beta_0001.nii,1'
+                                             'E:\TDT_MRI\16\outputs\LOCA_contrast\baseline\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\17\outputs\LOCA_contrast\baseline\beta_0001.nii,1'
+                                             'E:\TDT_MRI\18\outputs\LOCA_contrast\baseline\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\19\outputs\LOCA_contrast\baseline\beta_0001.nii,1'
+                                             'E:\TDT_MRI\20\outputs\LOCA_contrast\baseline\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\21\outputs\LOCA_contrast\baseline\beta_0001.nii,1'
+                                             'E:\TDT_MRI\22\outputs\LOCA_contrast\baseline\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\23\outputs\LOCA_contrast\baseline\beta_0001.nii,1'
+                                             'E:\TDT_MRI\24\outputs\LOCA_contrast\baseline\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\1\outputs\LOCA_contrast\conclusion\beta_0001.nii,1'
+                                             'E:\TDT_MRI\2\outputs\LOCA_contrast\conclusion\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\3\outputs\LOCA_contrast\conclusion\beta_0001.nii,1'
+                                             'E:\TDT_MRI\4\outputs\LOCA_contrast\conclusion\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\5\outputs\LOCA_contrast\conclusion\beta_0001.nii,1'
+                                             'E:\TDT_MRI\6\outputs\LOCA_contrast\conclusion\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\7\outputs\LOCA_contrast\conclusion\beta_0001.nii,1'
+                                             'E:\TDT_MRI\8\outputs\LOCA_contrast\conclusion\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\9\outputs\LOCA_contrast\conclusion\beta_0001.nii,1'
+                                             'E:\TDT_MRI\10\outputs\LOCA_contrast\conclusion\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\11\outputs\LOCA_contrast\conclusion\beta_0001.nii,1'
+                                             'E:\TDT_MRI\12\outputs\LOCA_contrast\conclusion\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\13\outputs\LOCA_contrast\conclusion\beta_0001.nii,1'
+                                             'E:\TDT_MRI\14\outputs\LOCA_contrast\conclusion\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\15\outputs\LOCA_contrast\conclusion\beta_0001.nii,1'
+                                             'E:\TDT_MRI\16\outputs\LOCA_contrast\conclusion\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\17\outputs\LOCA_contrast\conclusion\beta_0001.nii,1'
+                                             'E:\TDT_MRI\18\outputs\LOCA_contrast\conclusion\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\19\outputs\LOCA_contrast\conclusion\beta_0001.nii,1'
+                                             'E:\TDT_MRI\20\outputs\LOCA_contrast\conclusion\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\21\outputs\LOCA_contrast\conclusion\beta_0001.nii,1'
+                                             'E:\TDT_MRI\22\outputs\LOCA_contrast\conclusion\flipped_beta_0002.nii,1'
+                                             'E:\TDT_MRI\23\outputs\LOCA_contrast\conclusion\beta_0001.nii,1'
+                                             'E:\TDT_MRI\24\outputs\LOCA_contrast\conclusion\flipped_beta_0002.nii,1'
+                                             };
+%%
+matlabbatch{1}.spm.tools.swe.smodel.ciftiAdditionalInfo.ciftiGeomFile = struct('brainStructureLabel', {}, 'geomFile', {}, 'areaFile', {});
+matlabbatch{1}.spm.tools.swe.smodel.ciftiAdditionalInfo.volRoiConstraint = 1;
+matlabbatch{1}.spm.tools.swe.smodel.giftiAdditionalInfo.areaFileForGiftiInputs = {};
+matlabbatch{1}.spm.tools.swe.smodel.type.classic.ss = 4;
+matlabbatch{1}.spm.tools.swe.smodel.type.classic.dof_cl = 0;
+%%
+matlabbatch{1}.spm.tools.swe.smodel.subjects = [1
+                                                2
+                                                3
+                                                4
+                                                5
+                                                6
+                                                7
+                                                8
+                                                9
+                                                10
+                                                11
+                                                12
+                                                13
+                                                14
+                                                15
+                                                16
+                                                17
+                                                18
+                                                19
+                                                20
+                                                21
+                                                22
+                                                23
+                                                24
+                                                1
+                                                2
+                                                3
+                                                4
+                                                5
+                                                6
+                                                7
+                                                8
+                                                9
+                                                10
+                                                11
+                                                12
+                                                13
+                                                14
+                                                15
+                                                16
+                                                17
+                                                18
+                                                19
+                                                20
+                                                21
+                                                22
+                                                23
+                                                24
+                                                ];
+%%
+%%
+matlabbatch{1}.spm.tools.swe.smodel.cov(1).c = [1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0];
+%%
+matlabbatch{1}.spm.tools.swe.smodel.cov(1).cname = 'pre';
+%%
+matlabbatch{1}.spm.tools.swe.smodel.cov(2).c = [0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                0
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1
+                                                1];
+%%
+matlabbatch{1}.spm.tools.swe.smodel.cov(2).cname = 'post';
+%%
+matlabbatch{1}.spm.tools.swe.smodel.multi_cov = struct('files', {});
+matlabbatch{1}.spm.tools.swe.smodel.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.tools.swe.smodel.masking.im = 1;
+matlabbatch{1}.spm.tools.swe.smodel.masking.em = {'E:\2ndlevel\masks\posterioccipital_mask.nii,1'};
+matlabbatch{1}.spm.tools.swe.smodel.WB.WB_yes.WB_ss = 4;
+matlabbatch{1}.spm.tools.swe.smodel.WB.WB_yes.WB_nB = 999;
+matlabbatch{1}.spm.tools.swe.smodel.WB.WB_yes.WB_SwE = 0;
+matlabbatch{1}.spm.tools.swe.smodel.WB.WB_yes.WB_stat.WB_T.WB_T_con = [1 -1];
+matlabbatch{1}.spm.tools.swe.smodel.WB.WB_yes.WB_infType.WB_clusterwise.WB_clusThresh = 0.001;
+matlabbatch{1}.spm.tools.swe.smodel.WB.WB_yes.WB_infType.WB_clusterwise.WB_inputType.WB_img = 0;
+matlabbatch{1}.spm.tools.swe.smodel.globalc.g_omit = 1;
+matlabbatch{1}.spm.tools.swe.smodel.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.tools.swe.smodel.globalm.glonorm = 1;

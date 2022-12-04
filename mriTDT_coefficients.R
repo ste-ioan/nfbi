4cb701e4772ce4d5d8388339550d6320e9590dc3
ste-ioan <72309874+ste-ioan@users.noreply.github.com>
2022-12-04 12:34:10 +0100

    Add files via upload

---
+++ mriTDT_coefficients.R
@@ -0,0 +1,26 @@
+library(gamlj)
+
+data  <- readRDS("~/jamovi analyses/mriTDT/complete_dataset_MRItdt.rds")
+
+# for some reason block is read as numerical, convert it to category
+data$block<-as.factor(data$block)
+
+modello <- gamlj::gamljGlmMixed(formula = ACCtar ~ 1 + Quadrante + sessione + block + Quadrante:sessione+( 1 + Quadrante + sessione + Quadrante:sessione| subnumber ),
+                                data = data,
+                                contrasts = list(
+                                    list(
+                                        var="Quadrante",
+                                        type="simple"),
+                                    list(
+                                        var="sessione",
+                                        type="simple"),
+                                    list(
+                                        var="block",
+                                        type="repeated")),
+                                plotHAxis = sessione,
+                                plotSepLines = Quadrante,
+                                plotError = "ci")
+
+# same result as jamovi, now get subjects coefficients
+library(lme4)
+ranef(modello$model)
